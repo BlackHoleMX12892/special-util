eea7f88426117e1537468cd24ba88ddcb52a2acc
BlackHoleMX12892 <ikerpcamposr1283456@gmail.com>
2026-06-05 09:34:54 -0600

    Ready for first pre-release

--- src/SuDataHandler.cpp
+++ src/SuDataHandler.cpp
@@ -26,19 +26,21 @@ void SuData::getStorage() {
 }
 
 void SuData::saveToStorage(std::vector<std::string> order) {
-    Mod::get()->setSavedValue<std::string>("1", order[0]);
-    Mod::get()->setSavedValue<std::string>("2", order[1]);
-    Mod::get()->setSavedValue<std::string>("3", order[2]);
-    Mod::get()->setSavedValue<std::string>("4", order[3]);
-    Mod::get()->setSavedValue<std::string>("5", order[4]);
-    Mod::get()->setSavedValue<std::string>("6", order[5]);
-    Mod::get()->setSavedValue<std::string>("7", order[6]);
-    Mod::get()->setSavedValue<std::string>("8", order[7]);
-    Mod::get()->setSavedValue<std::string>("9", order[8]);
-    Mod::get()->setSavedValue<std::string>("10", order[9]);
-    Mod::get()->setSavedValue<std::string>("11", order[10]);
-    Mod::get()->setSavedValue<std::string>("12", order[11]);
-    Mod::get()->setSavedValue<std::string>("13", order[12]);
-    Mod::get()->setSavedValue<std::string>("14", order[13]);
-    Mod::get()->setSavedValue<std::string>("15", order[14]);
+    if (!order.empty()) {
+        Mod::get()->setSavedValue<std::string>("1", order[0]);
+        Mod::get()->setSavedValue<std::string>("2", order[1]);
+        Mod::get()->setSavedValue<std::string>("3", order[2]);
+        Mod::get()->setSavedValue<std::string>("4", order[3]);
+        Mod::get()->setSavedValue<std::string>("5", order[4]);
+        Mod::get()->setSavedValue<std::string>("6", order[5]);
+        Mod::get()->setSavedValue<std::string>("7", order[6]);
+        Mod::get()->setSavedValue<std::string>("8", order[7]);
+        Mod::get()->setSavedValue<std::string>("9", order[8]);
+        Mod::get()->setSavedValue<std::string>("10", order[9]);
+        Mod::get()->setSavedValue<std::string>("11", order[10]);
+        Mod::get()->setSavedValue<std::string>("12", order[11]);
+        Mod::get()->setSavedValue<std::string>("13", order[12]);
+        Mod::get()->setSavedValue<std::string>("14", order[13]);
+        Mod::get()->setSavedValue<std::string>("15", order[14]);
+    }
 }
